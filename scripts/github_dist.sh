f541c5307cecf316eed46d2b0ee922c5059b2691
Jon Shallow <supjps-libcoap@jpshallow.com>
2026-07-31 11:50:43 +0100

    CI: Cache Contiki/LwIP/Zephyr downloads

--- scripts/github_dist.sh
+++ scripts/github_dist.sh
@@ -16,47 +16,59 @@ echo $ARCHIVE
 if test $err = 0 -a "x$ARCHIVE" != "x"; then
     DIR=`pwd`/`tar taf $ARCHIVE |cut -d/ -f1|head -1`
     tar xaf $ARCHIVE && cd $DIR
-    terr=$?
-    if [ $err = 0 ] ; then
-        err=$terr
+    err=$?
+    if [ $err != 0 ] ; then
+        exit 1
     fi
 
     # LwIP
+    cp -a $RUNNER_TEMP/download/lwip $DIR/examples/lwip
     make -C $DIR/examples/lwip EXTRA_CFLAGS=-Werror
-    terr=$?
-    if [ $err = 0 ] ; then
-        err=$terr
+    err=$?
+    if [ $err != 0 ] ; then
+        exit 1
     fi
 
     # Contiki
+    cp -a $RUNNER_TEMP/download/contiki-ng $DIR/examples/contiki
+    (cd $DIR/examples/contiki/contiki-ng/os/net/app-layer && rm -rf libcoap && ln -s ../../../../../.. libcoap)
     make -C $DIR/examples/contiki
-    terr=$?
-    if [ $err = 0 ] ; then
-        err=$terr
+    err=$?
+    if [ $err != 0 ] ; then
+        exit 1
     fi
 
     # RIOT
     make -C $DIR/examples/riot
-    terr=$?
-    if [ $err = 0 ] ; then
-        err=$terr
+    err=$?
+    if [ $err != 0 ] ; then
+        exit 1
+    fi
+
+    # Zephyr
+    cp -a $RUNNER_TEMP/download/zephyrproject $DIR/examples/zephyr
+    (cd $DIR/examples/zephyr/zephyrproject/zephyr/zephyr-sdk* ; cmake -P cmake/zephyr_sdk_export.cmake)
+    make -C $DIR/examples/zephyr EXTRA_CFLAGS=-Werror
+    err=$?
+    if [ $err != 0 ] ; then
+        exit 1
     fi
 
     # Standard build
     $DIR/configure $PREFIX --enable-tests  --enable-silent-rules --enable-documentation --enable-examples --disable-dtls && \
     make EXTRA_CFLAGS=-Werror && make install EXTRA_CFLAGS=-Werror
-    terr=$?
-    if [ $err = 0 ] ; then
-        err=$terr
+    err=$?
+    if [ $err != 0 ] ; then
+        exit 1
     fi
 
     # cmake
     cmake -E make_directory test-cmake
     cd test-cmake
     cmake .. -DENABLE_EXAMPLES=ON -DENABLE_TESTS=ON -DENABLE_DTLS=OFF -DENABLE_DOCS=OFF -DWARNING_TO_ERROR=ON
-    terr=$?
-    if [ $err = 0 ] ; then
-        err=$terr
+    err=$?
+    if [ $err != 0 ] ; then
+        exit 1
     fi
 fi
 
